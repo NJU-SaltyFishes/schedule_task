991cbed78517e8b5242966c7588a951aac935648
VanianJH <48745280+VanianJH@users.noreply.github.com>
2020-11-15 21:16:17 +0800

    Update redis.py

--- conf/redis.py
+++ conf/redis.py
@@ -1,15 +1,16 @@
 # coding:utf-8
 
 from redis import ConnectionPool, StrictRedis
-
+import conf.config as cf
 
 class Redis:
     def __init__(self):
         self.pool = ConnectionPool(
-            host="",
-            port=""
+            host=cf.REDIS_HOST,
+            port=cf.REDIS_PORT
         )
         self.redisTemplate = StrictRedis(connection_pool=self.pool)
 
 
 RedisTemplate = Redis().redisTemplate
+
