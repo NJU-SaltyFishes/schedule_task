36b7fef3338cffedc2ef341e03acdbb63e5c07c0
reverse-mA <anhao0429@163.com>
2020-11-18 21:59:49 +0800

    添加向redis定时写机构对应作者方法

--- job/load_affiliation_data.py
+++ job/load_affiliation_data.py
@@ -14,7 +14,8 @@ def load_affiliation_data_job():
         # TODO 加载异常 发邮件
 
     # TODO 数据异常 发邮件
-    if len(affiliationLoader.affiliation_ids) == 0 or len(affiliationLoader.related_article_dict) == 0:
+    if len(affiliationLoader.affiliation_ids) == 0 or len(affiliationLoader.related_article_dict) == 0\
+            or len(affiliationLoader.related_author_dict)==0:
         print("{} affiliation_total_count: {}, final_affiliation_count: {}".format(
             time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
             len(affiliationLoader.affiliation_ids),
